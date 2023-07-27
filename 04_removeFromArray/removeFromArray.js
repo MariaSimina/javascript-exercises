497b071e978ae2b17b6e1b5777a77b7d4d67ca75
Maria Simina <maria.simina_groza@yahoo.com>
2023-07-27 17:27:55 +0300

    Do forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    let newArray = [];
+    let elementsArray = Array.from(elements);
+    console.log(arr);
+    console.log(elements);
 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < elementsArray.length; j++) {
+            console.log(`arr ${arr[i]}`);
+            console.log(elementsArray[j]);
+            if (arr[i] === elementsArray[j]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
